a574bedde2fa8617d84ab5e246628bf59fa8e54d
Devgen <dev@generationyct.nl>
2020-06-11 15:30:19 +0200

    Added shopify buy and shopify theme manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,28 +35,15 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-shopify`,
+      resolve: `gatsby-theme-shopify-manager`,
       options: {
-        // The domain name of your Shopify shop. This is required.
-        // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
-        // 'gatsby-source-shopify-test-shop.myshopify.com'.
-        shopName: process.env.SHOP_NAME,
-
-        // An API access token to your Shopify shop. This is required.
-        // You can generate an access token in the "Manage private apps" section
-        // of your shop's Apps settings. In the Storefront API section, be sure
-        // to select "Allow this app to access your storefront data using the
-        // Storefront API".
-        // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
+        shopName: process.env.SHOP_NAME, // or custom domain
         accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
-
-        // Set verbose to true to display a verbose output on `npm run develop`
-        // or `npm run build`. This prints which nodes are being fetched and how
-        // much time was required to fetch and process the data.
-        // Defaults to true.
-        verbose: true,
+        shouldIncludeSourcePlugin: true, // default
+        shouldWrapRootElementWithProvider: false, // default
       },
     },
+
     {
       resolve: 'gatsby-plugin-root-import',
       options: {
@@ -71,8 +58,5 @@ module.exports = {
         respectDNT: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ],
 }
